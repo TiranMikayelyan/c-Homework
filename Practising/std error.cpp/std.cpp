58f82e4facb6a5fbbcc83f1306c49f8f315daf3d
TiranMikayelyan <146861058+TiranMikayelyan@users.noreply.github.com>
2025-10-30 23:35:15 +0400

    v1

---
+++ Practising/std error.cpp/std.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>     
+using namespace std;  
+
+class vector {        
+public:
+    vector() { cout << "My Vector!" << endl; }
+};
+
+int main() {
+    vector t;        
+    return 0;
+}
+
+
+//using namespace std-ի վատ կողմը։
